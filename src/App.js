e8bee3194d075bcc0670c65293acf20e899e466c
Michal Kowalczyk <mickowalczyk6@gmail.com>
2021-12-01 20:06:25 +0100

    add basename to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <BrowserRouter>
+      <BrowserRouter basename='/react-movie-browser'>
         <Switch>
           <Route path='/movies'>
             <MovieListPage trend={false} />

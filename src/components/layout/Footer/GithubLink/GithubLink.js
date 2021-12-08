784dcb749d033b132e6407d826539283e25f3edd
Michal Kowalczyk <mickowalczyk6@gmail.com>
2021-12-08 23:55:29 +0100

    update github link

--- src/components/layout/Footer/GithubLink/GithubLink.js
+++ src/components/layout/Footer/GithubLink/GithubLink.js
@@ -3,7 +3,7 @@ import { StyledLink } from "./GithubLink.styled.js"
 const GithubLink = () => {
   return (
     <StyledLink
-      href="https://github.com/michakow"
+      href="https://github.com/michakow/react-movie-browser"
       target="_blank"
     >
       GitHub
